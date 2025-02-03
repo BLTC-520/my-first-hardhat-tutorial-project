0a592ce39a86b8fabcdf0407aa5bf210ef2d6f79
BLTC-520 <szehaohar@1utar.my>
2025-02-03 16:47:35 +0800

    First Commit for Hardhat Project

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
-
-// The next line is part of the sample project, you don't need it in your
-// project. It imports a Hardhat task definition, that can be used for
-// testing the frontend.
-require("./tasks/faucet");
+require("dotenv").config(); // Load environment variables from .env
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+// Load environment variables manually
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || "";
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || "";
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: SEPOLIA_PRIVATE_KEY ? [SEPOLIA_PRIVATE_KEY] : [],
+    },
+  },
 };
